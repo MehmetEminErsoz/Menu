c1de276142ede87b1edba5b2bcf91d43aad282e6
mexmet03 <mexmet03@gmail.com>
2022-03-01 23:49:16 +0300

    Add missing Tables.

---
+++ Menu.Entities/OrderPackageProduct_C.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu.Entities
+{
+    public class OrderPackageProduct_C
+    {
+        public int Id { get; set; }
+
+        public int  IdOrder { get; set; }
+
+        public int IdMenuPackageProduct_C { get; set; }
+
+        public virtual MenuPackageProduct_C MenuPackageProduct_C { get;set; }
+
+        public virtual Order Order { get; set; }
+    }
+}
